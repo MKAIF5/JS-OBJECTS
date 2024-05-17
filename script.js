522ab455e8eb61038ad846f0512f9a7c2ef50e5b
mohammed kaif <mkaif555123@gmail.com>
2024-05-17 21:02:41 +0500

    object topic 1

---
+++ script.js
@@ -0,0 +1,10 @@
+const user = {
+  name: "kaif",
+  age: "15",
+  class: "9th",
+  passion: ["cricket" , "web develpment " , "gaming"],
+  city: "karachi",
+  friend: "?"
+};
+
+console.log(user.girlFriend);
