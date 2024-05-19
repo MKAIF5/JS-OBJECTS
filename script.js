667d91bb40323555b76a0f1c0a793a0c5d957785
mohammed kaif <mkaif555123@gmail.com>
2024-05-19 20:45:19 +0500

    objects again

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let sym = Symbol()
+let sym = Symbol();
 
 const user = {
   name: "kaif",
   sym: "mai acha larka hun ",
   age: "15",
   class: "9th",
-  passion: ["cricket" , "web develpment " , "gaming"],
+  passion: ["cricket", "web develpment ", "gaming"],
   city: "karachi",
-  "friend": "?"
+  friend: "?",
+  attendence: [false , 35 , true],
 };
 
-console.log(user.sym);
+console.log(user.attendence[2]);
+user.age = "16";
+console.log(user.age);
